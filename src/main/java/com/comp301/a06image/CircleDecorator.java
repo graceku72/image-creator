9e85a475cf4ca1f86de22f3836111ef2333df951
Grace Ku <gku@unc.edu>
2023-03-21 21:51:04 -0400

    testing adept try 5

--- src/main/java/com/comp301/a06image/CircleDecorator.java
+++ src/main/java/com/comp301/a06image/CircleDecorator.java
@@ -5,42 +5,43 @@ import java.awt.*;
 import static java.lang.Math.sqrt;
 
 public class CircleDecorator implements Image {
-    private Image image;
-    private int cx;
-    private int cy;
-    private int radius;
-    private Color color;
-    public CircleDecorator(Image image, int cx, int cy, int radius, Color color) {
-        if (radius < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.image = image;
-        this.cx = cx;
-        this.cy = cy;
-        this.radius = radius;
-        this.color = color;
-    }
+  private final Image image;
+  private final int cx;
+  private final int cy;
+  private final int radius;
+  private final Color color;
 
-    @Override
-    public Color getPixelColor(int x, int y) {
-        if (sqrt(Math.pow(cx - x, 2) + Math.pow(cy - y, 2)) < radius) {
-            return color;
-        }
-        return image.getPixelColor(x, y);
+  public CircleDecorator(Image image, int cx, int cy, int radius, Color color) {
+    if (radius < 0) {
+      throw new IllegalArgumentException();
     }
+    this.image = image;
+    this.cx = cx;
+    this.cy = cy;
+    this.radius = radius;
+    this.color = color;
+  }
 
-    @Override
-    public int getWidth() {
-        return radius * 2;
+  @Override
+  public Color getPixelColor(int x, int y) {
+    if (sqrt(Math.pow(cx - x, 2) + Math.pow(cy - y, 2)) < radius) {
+      return color;
     }
+    return image.getPixelColor(x, y);
+  }
 
-    @Override
-    public int getHeight() {
-        return radius * 2;
-    }
+  @Override
+  public int getWidth() {
+    return image.getWidth();
+  }
 
-    @Override
-    public int getNumLayers() {
-        return image.getNumLayers() + 1;
-    }
+  @Override
+  public int getHeight() {
+    return image.getHeight();
+  }
+
+  @Override
+  public int getNumLayers() {
+    return image.getNumLayers() + 1;
+  }
 }
