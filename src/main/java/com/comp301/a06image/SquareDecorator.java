ed3f922ec122444403993047d7ed9766a0486221
Grace Ku <gku@unc.edu>
2023-03-21 21:54:15 -0400

    testing adept try 6

--- src/main/java/com/comp301/a06image/SquareDecorator.java
+++ src/main/java/com/comp301/a06image/SquareDecorator.java
@@ -3,42 +3,43 @@ package com.comp301.a06image;
 import java.awt.*;
 
 public class SquareDecorator implements Image {
-    private Image image;
-    private int squareX;
-    private int squareY;
-    private int squareSize;
-    private Color color;
-    public SquareDecorator(Image image, int squareX, int squareY, int squareSize, Color color) {
-        if (image == null || squareSize < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.image = image;
-        this.squareX = squareX;
-        this.squareY = squareY;
-        this.squareSize = squareSize;
-        this.color = color;
-    }
+  private final Image image;
+  private final int squareX;
+  private final int squareY;
+  private final int squareSize;
+  private final Color color;
 
-    @Override
-    public Color getPixelColor(int x, int y) {
-        if (x < squareX || x > squareX + squareSize || y > squareY || y < squareY - squareSize) {
-            return image.getPixelColor(x, y);
-        }
-        return color;
+  public SquareDecorator(Image image, int squareX, int squareY, int squareSize, Color color) {
+    if (image == null || squareSize < 0) {
+      throw new IllegalArgumentException();
     }
+    this.image = image;
+    this.squareX = squareX;
+    this.squareY = squareY;
+    this.squareSize = squareSize;
+    this.color = color;
+  }
 
-    @Override
-    public int getWidth() {
-        return image.getWidth();
+  @Override
+  public Color getPixelColor(int x, int y) {
+    if (x < squareX || x > squareX + squareSize || y > squareY || y < squareY - squareSize) {
+      return image.getPixelColor(x, y);
     }
+    return color;
+  }
 
-    @Override
-    public int getHeight() {
+  @Override
+  public int getWidth() {
+    return image.getWidth();
+  }
+
+  @Override
+  public int getHeight() {
     return image.getHeight();
-    }
+  }
 
-    @Override
-    public int getNumLayers() {
-        return image.getNumLayers() + 1;
-    }
+  @Override
+  public int getNumLayers() {
+    return image.getNumLayers() + 1;
+  }
 }
