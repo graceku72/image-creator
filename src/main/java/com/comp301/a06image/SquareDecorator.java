0e276790ac92a50fd1052e8ce1e5d505ba9d509d
Grace Ku <gku@unc.edu>
2023-03-21 21:58:55 -0400

    testing adept try 8

--- src/main/java/com/comp301/a06image/SquareDecorator.java
+++ src/main/java/com/comp301/a06image/SquareDecorator.java
@@ -22,10 +22,7 @@ public class SquareDecorator implements Image {
 
   @Override
   public Color getPixelColor(int x, int y) {
-    if (x > squareX && x < squareX + squareSize) {
-      return color;
-    }
-    if (y < squareY && y > squareY - squareSize) {
+    if (x > squareX && x < squareX + squareSize && y < squareY && y > squareY - squareSize) {
       return color;
     }
     return image.getPixelColor(x, y);
