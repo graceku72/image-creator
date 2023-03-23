bb500e548d5979e83459bc02f0feaa0e0acfab5d
Grace Ku <gku@unc.edu>
2023-03-23 18:02:33 -0400

    testing zoom getPixelColor() try 6

--- src/main/java/com/comp301/a06image/ZoomDecorator.java
+++ src/main/java/com/comp301/a06image/ZoomDecorator.java
@@ -22,7 +22,7 @@ public class ZoomDecorator implements Image {
         if (x < 0 || x >= this.getWidth() || y < 0 || y >= this.getHeight()) {
             throw new IllegalArgumentException();
         }
-        Color originalColor = image.getPixelColor((int) Math.floor(x / multiplier), (int) Math.floor(y / multiplier));
+        Color originalColor = image.getPixelColor(Math.round(x / multiplier), Math.round(y / multiplier));
         return originalColor;
     }
 
